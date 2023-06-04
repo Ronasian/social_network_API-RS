29326a6c09b7d155d8a4b6fa7197a20c39de9d9a
Ronasian <119774818+Ronasian@users.noreply.github.com>
2023-06-03 21:57:24 -0700

    added all routes for user

---
+++ routes/api/user-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+// GET users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET single user by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('thoughts')
+      .populate('friends');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST new user
+router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT update user by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE remove user by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
